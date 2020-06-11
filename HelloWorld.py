1ea823c88dfeea45eaf34b9bd770004023925755
VivekTalatam <vivektalatam2000@gmail.com>
2020-06-11 16:21:58 +0530

    Second Git Comit, learning Git.

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,7 @@
 __author__ = "Zephyr"
 
-print("Hello World!")
+print("Hello World! This is a program by Zephyr for Anders.\n")
+
+name = input("\nPlease enter your name: ")
+
+print("\nHello there, " + name + ". It's a pleasure to meet you!")
